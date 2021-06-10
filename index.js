1a46676bfaf9c8bfa8ab5f0616057491e6fc49ce
ramonjaspers <ramonj360@gmail.com>
2021-06-10 17:26:05 +0200

    exercises finished

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 const { question } = require("readline-sync");
-const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
+const { getGuessProgress, isGameLost, isGameWon, isLetterValid, drawHangman } = require("./gamelogic");
 
 function game(word, guesses) {
-  console.log("Dit heb je tot nu toe geraden: ", guesses);
+  console.log("Dit heb je tot nu toe geraden: \n ", JSON.stringify(guesses));
+  const letter = question("Raad een letter: ").toLowerCase();
 
-  const letter = question("Raad een letter: ");
+  // Check if the given letter is valid, if not we log a message and restart the round
+  if (!isLetterValid(guesses, letter)) {
+    return game(word, guesses);
+  }
 
-  // voeg de geraden letter toe aan de array met guesses
+  // Add the guessed letter to the array of guesses
   guesses.push(letter);
 
-  // volgende ronde! we roepen game nog een keer aan
-  game(word, guesses);
-}
+  const voortgang = getGuessProgress(word, guesses);
+  // Check if the game is finished
+  const isGamelost = isGameLost(word, guesses);
+  if (!isGamelost && isGameWon(word, guesses)) {
+    console.log(`!!!Hoera Je hebt het woord: ${word} geraden!!!!`);
+    return;
+  } else if (isGamelost) {
+    console.log(` 
+    __________  
+    | /     |   
+    |/     _o_    
+    |       O    
+    |      / \\   
+    |             
+    ===========  
+    R.I.P. je bent dood man 
+`);
+    return;
+  }
+
+  // De ronde is nog niet voorbij dus tonen de de voortgang en roepen de game nog een keer aan voor 
+  console.log(`Dit heb je voortgang: ${voortgang}`);
+  drawHangman(word, guesses);
+
+  return game(word, guesses);
+};
 
 console.log(`
 __________  
-| /     |    ░██████╗░░█████╗░██╗░░░░░░██████╗░░░░░░██╗███████╗
-|/     _o_   ██╔════╝░██╔══██╗██║░░░░░██╔════╝░░░░░░██║██╔════╝
-|       O    ██║░░██╗░███████║██║░░░░░██║░░██╗░░░░░░██║█████╗░░
-|      / \\   ██║░░╚██╗██╔══██║██║░░░░░██║░░╚██╗██╗░░██║██╔══╝░░
-|            ╚██████╔╝██║░░██║███████╗╚██████╔╝╚█████╔╝███████╗
-===========  ░╚═════╝░╚═╝░░╚═╝╚══════╝░╚═════╝░░╚════╝░╚══════╝
+| /     |     ░░██████╗░░█████╗░██╗░░░░░░██████╗░░░░░░██╗███████╗░
+|/     _o_    ░██╔════╝░██╔══██╗██║░░░░░██╔════╝░░░░░░██║██╔════╝░
+|       O     ░██║░░██╗░███████║██║░░░░░██║░░██╗░░░░░░██║█████╗░░░
+|      / \\    ░██║░░╚██╗██╔══██║██║░░░░░██║░░╚██╗██╗░░██║██╔══╝░░░
+|             ░╚██████╔╝██║░░██║███████╗╚██████╔╝╚█████╔╝███████╗░
+===========   ░░╚═════╝░╚═╝░░╚═╝╚══════╝░╚═════╝░░╚════╝░╚══════╝░
 `);
 
 game("javascript", []);
